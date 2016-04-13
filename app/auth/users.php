176ecf7a80292b5283cc08e010ed346d1b151a67
hatef <hatef.icontact@gmail.com>
2016-04-13 17:50:26 +0300

    #changes in users,login,content

--- app/auth/users.php
+++ app/auth/users.php
@@ -1,7 +1,38 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: hatef
- * Date: 4/2/16
- * Time: 7:03 PM
- */
+set_include_path("../../");
+include "configs/autoload.php";
+
+
+$email=filter_input(INPUT_POST,"email");
+$password=filter_input(INPUT_POST,"password");
+
+
+$users=$em->getRepository("User");
+$user=null;
+$result=array();
+if($email!=""&&$password!="") {
+    $user = $users->findOneBy(array("email" => $email));
+    if(password_verify($password,$user->getPassword())){
+        $result=array("success"=>true);
+
+        $userRecords=$users->findAll();
+
+        echo json_encode(serializeDoc($userRecords));
+
+    }
+    else{
+        $result=array("success"=>false,"valid"=>true);
+    }
+}
+else{
+    $result=array("valid"=>false,"success"=>false);
+}
+
+
+echo json_encode($result);
+
+
+
+function serializeDoc($data){
+
+}
